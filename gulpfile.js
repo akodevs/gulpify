34e661019b0715c14ee0b46a98ff1a777622b4e8
Kurt Garcia <kurt.garcia@electronicrecyclers.com>
2016-10-19 16:57:09 -0700

    Initialized gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/**
+ * Overview Defines
+ * gulp install - establish app for development preview
+ * 				- installs npm and bower
+ *				- compiles jade inot .tmp
+ * 				- injects styles, vendor and app scripts and styles into client/index.html
+ */
+
+// Gulp dependencies
+var gulp = require('gulp');
+var rename = rquire('gulp-rename');
+
+// Build Dependencies
+var browserify = require('browserify');
+var uglify = require('uglify');
+
+// Style Dependencies
+var sass = require('gulp-sass');
+var predix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+// Development Dependecies
+var jsHint = require('gulp-jshit');
+
+// Test Depencies
+var mochaPhantomjs('gulp-mocha-phantomjs');
+
+
+
